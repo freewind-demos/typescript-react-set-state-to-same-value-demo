83df0571ab29032a4d217e9d46412d6fe2ae571a
Freewind <nowindlee@gmail.com>
2020-12-13 18:41:21 +0800

    typescript-react-set-state-to-same-value-demo

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-TypeScript React Hello World Demo
+TypeScript React Set State to Same Value Demo
 =================================
+
+有点奇怪，多次点击将state设成同一个值，只会渲染两次。有点难以理解，为什么不是一次或者无限次？
 
 ```
 npm install
@@ -7,3 +9,5 @@ npm run demo
 ```
 
 It will open page on browser automatically.
+
+点击按钮，看浏览器console输出
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,9 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
 type Props = {};
 
 export const Hello: FC<Props> = ({}) => {
+  console.log('### render Hello')
+
+  const [name, setName] = useState('')
   return <div>
-    <h1>Hello React</h1>
+    <h1>Hello {name}</h1>
+    <button onClick={() => setName('react')}>set state to same value</button>
   </div>;
 }
